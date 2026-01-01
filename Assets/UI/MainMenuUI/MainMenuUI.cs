a08091d31829f573238d9492c805a7bd70794e17
DmitriyKovriga <107250542+DmitriyKovriga@users.noreply.github.com>
2026-01-01 14:41:10 +0300

    AddMinMenu

---
+++ Assets/UI/MainMenuUI/MainMenuUI.cs
@@ -0,0 +1,52 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public UIDocument ui;
+    private Button startGameButton;
+    private Button exitButton;
+    private Button settingsButton;
+
+    public WindowView settingsWindow;
+
+    private WindowManager manager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnEnable()
+    {
+        manager = Object.FindFirstObjectByType<WindowManager>();
+
+        var root = ui.rootVisualElement;
+
+        startGameButton = root.Q<Button>("StartGameButton");
+        exitButton = root.Q<Button>("ExitButton");
+        settingsButton = root.Q<Button>("SettingsButton");
+
+        startGameButton.clicked += OnStartGameClicked;
+        exitButton.clicked += OnExitClicked;
+        settingsButton.clicked += OnSettingsClicked;
+    }
+
+    private void OnStartGameClicked()
+    {
+        Debug.Log("Переход на сцену игры");
+    }
+
+    private void OnSettingsClicked()
+    {
+        manager.OpenWindow(settingsWindow);
+    }
+
+    private void OnExitClicked()
+    {
+#if UNITY_EDITOR
+        Debug.Log("Exit Game");
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    
+}
